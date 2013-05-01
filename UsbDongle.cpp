0a45c839f8c8c010dc2c3b4f7ced0c2fa4c3cfbb
Erik Gilling <konkers@konkers.net>
2013-05-01 13:43:10 -0700

    licence documentation cleanup

--- UsbDongle.cpp
+++ UsbDongle.cpp
@@ -1,4 +1,3 @@
-//
 // Copyright (C) 2013 Erik Gilling
 // Based on code Copyright (C) 2012 The Android Open Source Project
 // All rights reserved.
@@ -25,7 +24,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 // OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 // SUCH DAMAGE.
-//
+
 #include <stdarg.h>
 #include <stdio.h>
 
